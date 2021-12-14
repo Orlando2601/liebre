e0733951f6b1b3f684f28aa87da6e35f5446cd1c
oswar <oswar.baez26@gmail.com>
2021-12-14 12:44:04 -0500

    [Register y login]

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ const port = 3000;
 app.use(express.static(publicPath));
 app.listen(port, ()=> {console.log('Servidor corriendo en puerto ' + port)});
 app.get('/home', (req, res)=>{ res.sendFile(path.join(__dirname, '/views/index.html'))});
-
+app.get('/register', (req, res)=>{ res.sendFile(path.join(__dirname, '/views/register.html'))});
+app.get('/login', (req, res)=>{ res.sendFile(path.join(__dirname, '/views/login.html'))});
