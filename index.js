f46ef91e70bcc23489f774a4daeac9a678438cab
oswar <oswar.baez26@gmail.com>
2021-12-15 14:47:20 -0500

    [Organización y btn logo]

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ app.listen(port, ()=> {console.log('Servidor corriendo en puerto ' + port)});
 app.get('/home', (req, res)=>{ res.sendFile(path.join(__dirname, '/views/index.html'))});
 app.get('/register', (req, res)=>{ res.sendFile(path.join(__dirname, '/views/register.html'))});
 app.get('/login', (req, res)=>{ res.sendFile(path.join(__dirname, '/views/login.html'))});
+app.get('/ofertas', (req, res)=>{ res.sendFile(path.join(__dirname, '/views/ofertas.html'))});
