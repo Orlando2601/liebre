b67c2f996dcd4e3c9c971be4552e25e46a3b050d
oswar <oswar.baez26@gmail.com>
2021-12-17 23:53:29 -0500

    [update]

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 /* app.listen(port, ()=> {console.log('Servidor corriendo en puerto ' + port)}); */
 app.listen(process.env.PORT || 3000, () => { console.log('Servidor corriendo en el puerto 3000');});/* Configuracion heroku */
-app.get('/home', (req, res)=>{ res.sendFile(path.resolve(__dirname, './views/index.html'))});
-app.post('/home', (req, res)=>{ res.sendFile(path.resolve(__dirname, './views/index.html'))});
+app.get('/', (req, res)=>{ res.sendFile(path.resolve(__dirname, './views/index.html'))});
+app.post('/', (req, res)=>{ res.sendFile(path.resolve(__dirname, './views/index.html'))});
 app.get('/register', (req, res)=>{ res.sendFile(path.resolve(__dirname, './views/register.html'))});
 app.get('/login', (req, res)=>{ res.sendFile(path.resolve(__dirname, './views/login.html'))});
 app.get('/ofertas', (req, res)=>{ res.sendFile(path.resolve(__dirname, './views/ofertas.html'))});
