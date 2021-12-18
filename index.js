20cf6df43d6a883e5ad24b8ea8ff093a9be77cc5
oswar <oswar.baez26@gmail.com>
2021-12-17 21:47:08 -0500

    [heroku 3]

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const publicPath = path.resolve(__dirname, './public');
 /* const port = 3000; */
 app.use(express.static(publicPath));
 /* app.listen(port, ()=> {console.log('Servidor corriendo en puerto ' + port)}); */
-const PORT = process.env.PORT || 3000; /* Configuracion heroku */
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${ PORT }`)
-});/* Configuracion heroku */
+app.listen(process.env.PORT || 3000, () => { console.log('Servidor corriendo en el puerto 3000');});/* Configuracion heroku */
 app.get('/home', (req, res)=>{ res.sendFile(path.join(__dirname, '/views/index.html'))});
 app.get('/register', (req, res)=>{ res.sendFile(path.join(__dirname, '/views/register.html'))});
 app.get('/login', (req, res)=>{ res.sendFile(path.join(__dirname, '/views/login.html'))});
